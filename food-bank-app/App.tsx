fb1facfd3169dd912242f92bb623b9d7ddd0df24
Carlos Rodriguez <carlos.rodriguez@100ladrillos.com>
2022-10-20 10:02:40 -0500

    Created basic input screen

--- food-bank-app/App.tsx
+++ food-bank-app/App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { 
+  Alert,
+  Button,
+  StyleSheet,
+  Text, 
+  TextInput,
+  View
+} from 'react-native';
 
+// libreria material ui 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Bienvenido</Text>
+      <TextInput
+        style={styles.textInput}
+        value={email} 
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        style={styles.textInput}
+        value={password} 
+        onChangeText={text => setPassword(text)}
+      />
+      <Button
+        title="Iniciar Sesión"
+        onPress={() => Alert.alert('navigate to home')}
+      />
     </View>
   );
 }
@@ -16,5 +40,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    width: 200,
+    margin: 10,
+    borderWidth: 1,
   },
 });
