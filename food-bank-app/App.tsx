d926ff17453b186c685a9aa798b5f4211e1e51c7
BrandonMagana <magana.brandon12@outlook.com>
2022-10-29 12:58:45 -0500

    feat(expo)Created basic AuthContext and dummy navigation

--- food-bank-app/App.tsx
+++ food-bank-app/App.tsx
@@ -1,84 +1,10 @@
-import { createContext, useContext, useState } from 'react';
-import { 
-  Button,
-  StyleSheet,
-  Text, 
-  TextInput,
-  View
-} from 'react-native';
+import { AuthProvider } from './contexts/AuthContext';
+import Main from './Main';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
-
-interface AuthContextValue  {
-  isAuthenticated: boolean;
-  setIsAuthenticated: (isAuthenticated: boolean) => void;
-};
-
-const AuthContext = createContext({} as AuthContextValue);
-
-function Login({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const { setIsAuthenticated } = useContext(AuthContext);
-
+export default function App(){    
   return (
-    <View style={styles.container}>
-      <Text>Bienvenido</Text>
-      <TextInput
-        style={styles.textInput}
-        value={email} 
-        onChangeText={text => setEmail(text)}
-      />
-      <TextInput
-        style={styles.textInput}
-        value={password} 
-        onChangeText={text => setPassword(text)}
-      />
-      <Button
-        title="Iniciar Sesión"
-        onPress={() => setIsAuthenticated(true)}
-      />
-    </View>
-  );
+    <AuthProvider>
+        <Main/>
+    </AuthProvider>
+  )
 }
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
-  );
-}
-
-// libreria material ui 
-export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {!isAuthenticated ? <Stack.Screen name="Login" component={Login} /> : <Stack.Screen name="Home" component={HomeScreen} />}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AuthContext.Provider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textInput: {
-    width: 200,
-    margin: 10,
-    borderWidth: 1,
-  },
-});
---
+++ food-bank-app/styles/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textInput: {
+      width: 200,
+      margin: 10,
+      borderWidth: 1,
+    },
+});
