e53b91c15c0440a93b263ebee80aef68e02a4007
antonieto <antonioch.mon@gmail.com>
2022-11-01 21:42:07 -0600

    ref(DB models): split Contributor data to another model

--- server/food_bank_app/users/models.py
+++ server/food_bank_app/users/models.py
@@ -4,17 +4,19 @@ from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
-
 class Contributor(AbstractUser):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    email = models.EmailField(null=False, unique=True,
+                              editable=False, max_length=20)
+
+class ContributorData(models.Model):
+    contributor_id = models.OneToOneField(
+        Contributor, on_delete=models.CASCADE, primary_key=True)
 
     first_name = models.CharField(null=False, blank=False, max_length=20)
     middle_name = models.CharField(null=False, blank=True, max_length=20)
     surname = models.CharField(null=False, blank=False, max_length=20)
     second_surname = models.CharField(null=False, blank=False, max_length=20)
-
     phone = PhoneNumberField(null=False, blank=False, unique=False)
-    email = models.EmailField(null=False, unique=True,
-                              editable=False, max_length=20)
+    
     rfc = models.CharField(null=True, blank=False, max_length=13)
